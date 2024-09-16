64811f33c02fa90ff82f692fb987aeda8f56c53d
Ayambakar57 <muhammadrafifazka27@gmail.com>
2024-09-16 21:46:34 +0700

    test brach

--- lib/screen/bmi_screen.dart
+++ lib/screen/bmi_screen.dart
@@ -27,7 +27,7 @@ class _BMIScreenState extends ConsumerState<BMIScreen> {
         title: Text('BMI Calculator'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(18.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
